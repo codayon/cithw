7e897b42f371368492e09db665ffa24cca64a8cd
Reshad Ahammed Ayon <codayon@proton.me>
2025-12-24 02:57:03 +0600

    feat: add a basic express/mongoose app

--- 78/index.js
+++ 78/index.js
@@ -1,13 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+import { User } from "./userSchema.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect", err));
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Something went wrong on our end" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`you can see my secret mongodb url at http://localhost:${port}/`);
+  console.log(`Server listening at http://localhost:${port}/`);
 });
